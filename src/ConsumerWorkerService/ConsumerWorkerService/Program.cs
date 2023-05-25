7a8452cbdff9760336c0f6a3879ff73fffe665bd
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2023-05-24 22:51:03 -0300

    Configuração da Program

--- src/ConsumerWorkerService/ConsumerWorkerService/Program.cs
+++ src/ConsumerWorkerService/ConsumerWorkerService/Program.cs
@@ -1,10 +1,27 @@
-using ConsumerWorkerService;
+using ConsumerWorkerService.Consumer;
+using MassTransit;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
-    {
-        services.AddHostedService<Worker>();
-    })
-    .Build();
+var host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddMassTransit(configure =>
+                    {
+                        configure.AddConsumer<MensagemConsumer>();
+
+                        configure.UsingRabbitMq((context, cfg) =>
+                        {
+                            cfg.Host(new Uri("rabbitmq://localhost"), host =>
+                            {
+                                host.Username("guest");
+                                host.Password("guest");
+                            });
+
+                            cfg.ConfigureEndpoints(context);
+                        });
+                    });
+
+                    services.AddMassTransitHostedService();
+                })
+                .Build();
 
 await host.RunAsync();
