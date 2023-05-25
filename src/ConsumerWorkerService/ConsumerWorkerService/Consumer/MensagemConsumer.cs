0c31c651ca507e878c3c102650964ea0a161571f
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2023-05-24 22:46:10 -0300

    Criação do Consumidor

---
+++ src/ConsumerWorkerService/ConsumerWorkerService/Consumer/MensagemConsumer.cs
@@ -0,0 +1,19 @@
+﻿using MassTransit;
+using MassTransitExchange;
+
+namespace ConsumerWorkerService.Consumer
+{
+    public class MensagemConsumer : IConsumer<MensagemEntity>
+    {
+        public Task Consume(ConsumeContext<MensagemEntity> context)
+        {
+            var message = context.Message;
+            
+            // Aqui entra a regra após o consumo da mensagem...
+
+            Console.WriteLine($"Received message {message.IdMensagem}: {message.Descricao}.");
+
+            return Task.CompletedTask;
+        }
+    }
+}
