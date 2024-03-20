ba9d61af12e956f9aea37ea313c03708dc48441a
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2024-03-20 11:21:53 -0300

    Adiciona configuração de retentativas e circuit breaker

--- src/ConsumerWorkerService/ConsumerWorkerService/Program.cs
+++ src/ConsumerWorkerService/ConsumerWorkerService/Program.cs
@@ -1,27 +1,98 @@
+using ConsumerWorkerService;
 using ConsumerWorkerService.Consumer;
 using MassTransit;
+using MassTransitExchange;
 
-var host = Host.CreateDefaultBuilder(args)
-                .ConfigureServices((hostContext, services) =>
+var builder = new ConfigurationBuilder().AddJsonFile($"appsettings.json", true, true);
+var config = builder.Build();
+
+IHost host = Host.CreateDefaultBuilder(args)
+
+.UseWindowsService(options =>
+{
+    options.ServiceName = "Consumidor para cancelar envelope Docusign";
+})
+
+.ConfigureServices(services =>
+{
+    //services.SetupIocDI(); Injeção de depêndencia
+
+    services.AddMassTransit(cfg =>
+    {
+        cfg.AddConsumer<MensagemConsumer>();
+
+        cfg.UsingRabbitMq((context, rabbitMqConfiguration) =>
+        {
+            rabbitMqConfiguration.UseTimeout(x =>
+            {
+                x.Timeout = TimeSpan.FromSeconds(config.GetValue<int>("Servicos:RabbitMq:Timeout"));
+            });
+
+            rabbitMqConfiguration.Host(config.GetValue<string>("Servicos:RabbitMq:Host"), config.GetValue<string>("Servicos:RabbitMq:VHost"), x =>
+            {
+                var tls = config.GetValue<bool>("Servicos:RabbitMq:Tls");
+
+                if (tls)
                 {
-                    services.AddMassTransit(configure =>
+                    x.UseSsl(s =>
                     {
-                        configure.AddConsumer<MensagemConsumer>();
-
-                        configure.UsingRabbitMq((context, cfg) =>
-                        {
-                            cfg.Host(new Uri("rabbitmq://localhost"), host =>
-                            {
-                                host.Username("guest");
-                                host.Password("guest");
-                            });
-
-                            cfg.ConfigureEndpoints(context);
-                        });
+                        s.Protocol = System.Security.Authentication.SslProtocols.Tls12;
                     });
+                }
 
-                    services.AddMassTransitHostedService();
-                })
-                .Build();
+                x.Username(config.GetValue<string>("Servicos:RabbitMq:User"));
+                x.Password(config.GetValue<string>("Servicos:RabbitMq:Password"));
+            });
 
-await host.RunAsync();
+            rabbitMqConfiguration.ReceiveEndpoint(config["Servicos:RabbitMq:Fila"], e =>
+            {
+                e.SetQuorumQueue();
+                e.ConfigureConsumeTopology = false;
+                e.ConcurrentMessageLimit = config.GetValue<int>("Servicos:RabbitMq:MsgConsumidasEmParalelo");
+                e.PrefetchCount = config.GetValue<int>("Servicos:RabbitMq:MsgConsumidasEmParalelo");
+                e.ThrowOnSkippedMessages();
+
+                e.Bind<MensagemEntity>(x =>
+                {
+                    x.ExchangeType = "direct";
+                    x.RoutingKey = config["Servicos:RabbitMq:RoutingKey"];
+                });
+
+                // Retry: Esta função é nativa do masstransit
+
+                e.UseMessageRetry(x =>
+                {
+                    int qtdRetentativas = config.GetValue<int>("Servicos:Retry:Tentativas");
+                    int tempoPrimeiraRetentativa = config.GetValue<int>("Servicos:Retry:PrimeiraRetentativa");
+                    int incrementoRetentativa = config.GetValue<int>("Servicos:Retry:IncrementoRetentativa");
+
+                    x.Incremental(
+                        retryLimit: qtdRetentativas,
+                        initialInterval: TimeSpan.FromSeconds(tempoPrimeiraRetentativa),
+                        intervalIncrement: TimeSpan.FromSeconds(incrementoRetentativa));
+                });
+
+                // Circuit Breaker:
+                e.UseKillSwitch(x =>
+                {
+                    int porcentagemDeErros = config.GetValue<int>("Servicos:CircuitBreaker:PorcentagemDeErros");
+                    int tempoCircuitoAberto = config.GetValue<int>("Servicos:CircuitBreaker:TempoAberturaMinutos");
+                    int tempoTracking = config.GetValue<int>("Servicos:CircuitBreaker:TempoDeTranckingEmMinutos");
+                    int activationThreshold = config.GetValue<int>("Servicos:CircuitBreaker:ActivationThreshold");
+
+                    x.TrackingPeriod = TimeSpan.FromMinutes(tempoTracking);
+                    x.TripThreshold = porcentagemDeErros;
+                    x.SetActivationThreshold(activationThreshold);
+                    x.SetRestartTimeout(TimeSpan.FromMinutes(tempoCircuitoAberto));
+                });
+
+                e.ConfigureConsumer<MensagemConsumer>(context);
+            });
+        });
+    });
+
+    services.AddHostedService<Worker>();
+
+}).Build();
+
+host.Run();
