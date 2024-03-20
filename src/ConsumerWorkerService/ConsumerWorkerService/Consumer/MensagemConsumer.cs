f19a59e806002043e62c5bc5c6de7c0f7f1b82a7
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2024-03-20 11:31:20 -0300

    Adiciona método para retentativa e envio a DLQ

--- src/ConsumerWorkerService/ConsumerWorkerService/Consumer/MensagemConsumer.cs
+++ src/ConsumerWorkerService/ConsumerWorkerService/Consumer/MensagemConsumer.cs
@@ -5,15 +5,52 @@ namespace ConsumerWorkerService.Consumer
 {
     public class MensagemConsumer : IConsumer<MensagemEntity>
     {
-        public Task Consume(ConsumeContext<MensagemEntity> context)
+        private readonly IConfiguration _config;
+
+        public MensagemConsumer(IConfiguration config)
         {
-            var message = context.Message;
-            
-            // Aqui entra a regra após o consumo da mensagem...
+            _config = config;
+        }
 
-            Console.WriteLine($"Received message {message.IdMensagem}: {message.Descricao}.");
+        public async Task Consume(ConsumeContext<MensagemEntity> context)
+        {
+            try
+            {
+                var message = context.Message;
 
-            return Task.CompletedTask;
+                // Aqui entra a regra após o consumo da mensagem...
+
+                Console.WriteLine($"Received message {message.IdMensagem}: {message.Descricao}.");
+
+                await context.ConsumeCompleted;
+            }
+            catch (Exception ex)
+            {
+                await RetentativaEnvioDLQAsync(context, ex);
+            }
+        }
+
+        private async Task RetentativaEnvioDLQAsync(ConsumeContext<MensagemEntity> context, Exception exception)
+        {
+            if (MaximoRetentativasPermitidas(context))
+            {
+                var loggerTrace = Guid.NewGuid();
+
+                Console.WriteLine($"ERRO MSG ENVIADA PARA FILA DLQ, LOG TRACE: {loggerTrace}");
+
+                var msg = $"ERRO Id mensagem: {context.Message.IdMensagem}, Mensagem: {context.Message.Descricao} EXCEPTION:{exception.Message}";
+
+                //await _logger.SalvarLogErroAsync(msg, exception, loggerTrace);
+            }
+
+            throw exception;
+        }
+        private bool MaximoRetentativasPermitidas(ConsumeContext<MensagemEntity> context)
+        {
+            var retentativas = _config.GetValue<int>("Servicos:Retry:Tentativas");
+            int retentaivasPermitidas = retentativas - 1;
+
+            return context.GetRetryCount() == retentaivasPermitidas;
         }
     }
 }
